c48d8c284d4d42206a2e9ce7984656428208d661
hyewonJoo <run98@naver.com>
2022-04-25 01:16:45 +0900

    add baekjoon example for java

---
+++ Q30.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Q30 {
+	/*
+	 * 상근이의 동생 상수는 수학을 정말 못한다. 상수는 숫자를 읽는데 문제가 있다. 이렇게 수학을 못하는 상수를 위해서 상근이는 수의 크기를 비교하는 문제를 내주었다. 상근이는 세 자리 수 두 개를 칠판에 써주었다. 그 다음에 크기가 큰 수를 말해보라고 했다.
+	
+	상수는 수를 다른 사람과 다르게 거꾸로 읽는다. 예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다. 따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
+	
+	두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
+	
+	첫째 줄에 상근이가 칠판에 적은 두 수 A와 B가 주어진다.
+	 두 수는 같지 않은 세 자리 수이며, 0이 포함되어 있지 않다.
+	 */
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("같지않은 세자리 수 두개를 입력하세요");
+		String num1 = scan.next();
+		String num2 = scan.next();
+		String result = "";
+
+
+		for (int i = 2; i >= 0; i--) {
+			if (num1.charAt(i) > num2.charAt(i)) {
+				result = String.format("%c%c%c", num1.charAt(2), num1.charAt(1), num1.charAt(0));
+				break;
+			} else if (num1.charAt(i) < num2.charAt(i)) {
+				result = String.format("%c%c%c", num2.charAt(2), num2.charAt(1), num2.charAt(0));
+				break;
+			}
+		}
+
+		System.out.println(result);
+
+	}
+}
